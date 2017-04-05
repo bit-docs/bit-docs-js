c58301593adafe139f73b727002bf7c50f22ffbd
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-05 10:50:10 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -2,6 +2,16 @@ var tags = require("./tags/tags");
 var processJavaScript = require("./process/javascript");
 var path = require("path");
 
+/**
+ * @module {function} bit-docs-js
+ * @parent plugins
+ *
+ * @description A collection of tags, templates, and basic styles for JavaScript applications.
+ *
+ * @body
+ *
+ * TBD
+ */
 module.exports = function(bitDocs){
     // register your tags
     bitDocs.register("tags", tags);

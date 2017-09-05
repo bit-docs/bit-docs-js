0aab1fe8eab8f489c72586492c791c4f27f59aea
Joel Kuzmarski <leoj3n@gmail.com>
2017-09-05 09:29:48 -0500

    Add base styles for signature template

--- bit-docs.js
+++ bit-docs.js
@@ -5,6 +5,7 @@ var path = require("path");
 /**
  * @module {function} bit-docs-js
  * @parent plugins
+ * @group bit-docs-js/static static
  *
  * @description A collection of tags, templates, and basic styles for JavaScript applications.
  *
@@ -13,12 +14,16 @@ var path = require("path");
  * TBD
  */
 module.exports = function(bitDocs){
+    var pkg = require("./package.json");
+    var dependencies = {};
+    dependencies[pkg.name] = pkg.version;
+
     // register your tags
     bitDocs.register("tags", tags);
     bitDocs.register("processor", processJavaScript);
 
     bitDocs.register("html", {
-        templates: path.join(__dirname, "templates")
+        templates: path.join(__dirname, "templates"),
+        dependencies: dependencies
     });
-
 };
---
+++ index.js
@@ -0,0 +1,1 @@
+require('./static/styles/styles.less');

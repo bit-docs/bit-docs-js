257ca2949e75306ef4c8af31280202b921e0b8df
Joel Kuzmarski <leoj3n@gmail.com>
2017-09-06 01:28:21 -0500

    Document static file index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+/**
+ * @parent bit-docs-js/static
+ * @module bit-docs-js/index.js
+ * 
+ * Main front end JavaScript file for static portion of this plugin.
+ * 
+ * Simply requires [bit-docs-js/static/styles/signature.less].
+ */
 require('./static/styles/signature.less');
